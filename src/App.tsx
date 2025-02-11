5a8d6148d8d2794bd177276ae6c3b69e592365e6
ValeLan <vale.lanf@gmail.com>
2025-02-11 19:30:19 -0300

    Cree un compunente Form y Modulos de estilos para el mismo y para App

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styles from "./App.module.css"
+import Form from "./form/Form"
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className={styles.title}>Buscador de Clima</h1>
+    <div className={styles.container}>
+      <Form/>
+      <p>2</p>
+    </div>
+    </>
+  )
+}
+
+export default App
